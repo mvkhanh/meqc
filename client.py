2198880fdb5396f9fd68b3fa7a77f283d2a7a2c1
mvk <mvkhanh100204@gmail.com>
2025-10-02 13:59:54 +0700

    .

--- client.py
+++ client.py
@@ -52,8 +52,7 @@ def main(args):
     detect_worker = DetectWorker(detector_path=args.face_detection_model, detect_every_n=args.den,
                                  face_score_thres=args.face_thres, agegender_path=args.agegender_model,
                                  width=args.width, height=args.height, in_q=in_q, out_q=out_q,
-                                 emotion_path=args.emotion_model, gaze_path=args.gaze_model,
-                                 )
+                                 emotion_path=args.emotion_model, gaze_path=args.gaze_model)
     
     capture_worker.start()
     detect_worker.start()
