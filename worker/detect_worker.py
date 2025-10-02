e831b383e02a1559341b26c40d495ee655f433f4
mvk <mvkhanh100204@gmail.com>
2025-10-02 14:13:43 +0700

    .

--- worker/detect_worker.py
+++ worker/detect_worker.py
@@ -123,7 +123,7 @@ class HailoInferProc(Process):
             vec = out_arrs[0].reshape(-1)
             if vec.size == 0:
                 return {"emotion": None, "emotion_conf": None}
-            prob = _softmax(vec)
+            prob = self._softmax(vec)
             idx = int(prob.argmax())
             conf = float(prob[idx])
             label = EMO_LABELS[idx] if idx < len(EMO_LABELS) else f"cls_{idx}"
@@ -149,7 +149,7 @@ class HailoInferProc(Process):
             age_raw = float(np.asarray(out_arrs[1]).squeeze())
             # prob_female = 1.0 / (1.0 + np.exp(-g))
             
-            probs = _softmax(g)
+            probs = self._softmax(g)
             prob_female = float(probs[1])
             if prob_female >= 0.5:
                 gender_label = "Female"
